706a094d9dff8c90804fae4d5a216075d4decf05
Romain Beucher <romainbeucher@thermo-geology.fr>
2014-10-01 22:14:34 +0200

    FFTPACK

---
+++ sources/FFTPACK/fftpack5.1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+mkdir temp
+cd temp
+f90split ../fftpack5.1.f90
+#
+for FILE in `ls -1 *.f90`;
+do
+  gfortran -c -O3 $FILE
+  if [ $? -ne 0 ]; then
+    echo "Errors compiling " $FILE
+    exit
+  fi
+done
+rm *.f90
+#
+ar cr libfftpack5.1.a *.o
+rm *.o
+#
+mv libfftpack5.1.a ~/lib/$ARCH
+cd ..
+rmdir temp
+#
+echo "Library installed as ~/lib/$ARCH/libfftpack5.1.a."
